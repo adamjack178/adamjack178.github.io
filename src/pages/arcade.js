203802a7945ba3ccc69b2c4d33db3be97f9d328d
Daniel Gonzalez <danielhumgon@gmail.com>
2023-02-01 16:29:32 -0400

    Fixed countdown timer

--- src/pages/arcade.js
+++ src/pages/arcade.js
@@ -3,51 +3,34 @@ import React, { useState, useEffect } from "react";
 import arcadeStyles from "./styles/arcade.module.scss";
 
 const Arcade = () => {
-    const [days, setDays] = useState(+localStorage.getItem('days') || 30);
-    const [hours, setHours] = useState(+localStorage.getItem('hours') || 0);
-    const [minutes, setMinutes] = useState(+localStorage.getItem('minutes') || 0);
-    const [seconds, setSeconds] = useState(+localStorage.getItem('seconds') || 0);
-    const [timeLeft, setTimeLeft] = useState(
-      (days * 86400) + (hours * 3600) + (minutes * 60) + seconds
-    );
-    const [tickets, setTickets] = useState(+localStorage.getItem('tickets') || 0);
-  
-    useEffect(() => {
-      localStorage.setItem('days', days);
-      localStorage.setItem('hours', hours);
-      localStorage.setItem('minutes', minutes);
-      localStorage.setItem('seconds', seconds);
-    }, [days, hours, minutes, seconds]);
-  
-    useEffect(() => {
-      localStorage.setItem('tickets', tickets);
-    }, [tickets]);
-  
-    useEffect(() => {
-      const intervalId = setInterval(() => {
-        setTimeLeft((timeLeft) => {
-          if (timeLeft === 0) {
-            clearInterval(intervalId);
-          }
-  
-          return timeLeft - 1;
-        });
-      }, 1000);
-  
-      return () => clearInterval(intervalId);
-    }, []);
-  
-    useEffect(() => {
-      setDays(Math.floor(timeLeft / 86400));
-      setHours(Math.floor((timeLeft % 86400) / 3600));
-      setMinutes(Math.floor(((timeLeft % 86400) % 3600) / 60));
-      setSeconds(Math.floor(((timeLeft % 86400) % 3600) % 60));
-    }, [timeLeft]);
-  
-    const handleBuyTicket = () => {
-      setTickets(tickets + 1);
+  const [date] = useState(new Date("2023-02-10"));
+  const [countdown, setCountdown] = useState({});
+  const [tickets, setTickets] = useState(0);
+
+  useEffect(() => {
+    const calculateCountdown = () => {
+      const difference = +date - +new Date();
+      let tempCountdown = {};
+
+      if (difference > 0) {
+        tempCountdown = {
+          days: Math.floor(difference / (1000 * 60 * 60 * 24)),
+          hours: Math.floor((difference / (1000 * 60 * 60)) % 24),
+          minutes: Math.floor((difference / 1000 / 60) % 60),
+          seconds: Math.floor((difference / 1000) % 60),
+        };
+      }
+
+      setCountdown(tempCountdown);
     };
-  
+
+    const intervalId = setInterval(calculateCountdown, 1000);
+    return () => clearInterval(intervalId);
+  }, [date]);
+
+  const handleBuyTicket = () => {
+    setTickets(tickets + 1);
+  };
 
   return (
     <div className={arcadeStyles.container}>
@@ -56,19 +39,19 @@ const Arcade = () => {
         <div className={arcadeStyles.timeBlocksSection}>
           <div className={arcadeStyles.timeBlock}>
             <h3>Days</h3>
-            <span>{days} :</span>
+            <span>{countdown.days} :</span>
           </div>
           <div className={arcadeStyles.timeBlock}>
             <h3>Hours</h3>
-            <span>{hours} :</span>
+            <span>{countdown.hours} :</span>
           </div>
           <div className={arcadeStyles.timeBlock}>
             <h3>Minutes</h3>
-            <span>{minutes} :</span>
+            <span>{countdown.minutes} :</span>
           </div>
           <div className={arcadeStyles.timeBlock}>
             <h3>Seconds</h3>
-            <span>{seconds}</span>
+            <span>{countdown.seconds}</span>
           </div>
         </div>
       </div>
